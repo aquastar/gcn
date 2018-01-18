8290b5010215446fd3b26ee17ed9730560dac3c1
aquastar <qqadd520@126.com>
2018-01-17 19:18:53 -0500

    update

--- gcn/train.py
+++ gcn/train.py
@@ -12,96 +12,6 @@ from sklearn.metrics import roc_curve, auc
 from gcn.models import GCN, MLP, RAT, RAT_ELEMENT, RAT_after_GCN
 from gcn.utils import *
 
-# Set random seed
-seed = 88
-np.random.seed(seed)
-tf.set_random_seed(seed)
-
-# Settings
-flags = tf.app.flags
-FLAGS = flags.FLAGS
-flags.DEFINE_string('dataset', 'simu', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed', 'simu'
-flags.DEFINE_string('model', 'gcn_cheby', 'Model string.')  # 'gcn', 'gcn_cheby', 'dense', 'rat'
-flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
-flags.DEFINE_integer('epochs', 200, 'Number of epochs to train.')
-flags.DEFINE_integer('hidden1', 16, 'Number of units in hidden layer 1.')
-flags.DEFINE_float('dropout', 0.5, 'Dropout rate (1 - keep probability).')
-flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
-flags.DEFINE_integer('early_stopping', 50, 'Tolerance for early stopping (# of epochs).')
-flags.DEFINE_integer('max_degree', 3, 'Maximum Chebyshev polynomial degree.')
-flags.DEFINE_integer('eig_dim', 400, 'Maximum eigen value number.')
-
-# Load data
-adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(FLAGS.dataset)
-
-support_inv = []
-approx_eigen = True
-
-# Some preprocessing
-features = preprocess_features(features)
-if FLAGS.model == 'gcn':
-    support = [preprocess_adj(adj)]
-    num_supports = 1
-    model_func = GCN
-    print('gcn')
-elif FLAGS.model == 'gcn_cheby':
-    support = chebyshev_polynomials(adj, FLAGS.max_degree)
-    support_inv = chebyshev_polynomials_inv(adj, FLAGS.max_degree)
-    num_supports = 1 + FLAGS.max_degree
-    model_func = GCN
-    print('gcn_cheby')
-elif FLAGS.model == 'dense':
-    support = [preprocess_adj(adj)]  # Not used
-    num_supports = 1
-    model_func = MLP
-    print('dense')
-elif FLAGS.model == 'rat':
-    support = normal_rational(adj, FLAGS.max_degree)
-    num_supports = 1 + FLAGS.max_degree
-    model_func = RAT
-    print('rational')
-elif FLAGS.model == 'rat_element':
-    support = element_rational(adj, FLAGS.max_degree, eig_dim=FLAGS.eig_dim)
-    num_supports = 1 + FLAGS.max_degree
-    model_func = RAT_ELEMENT
-    print('rational')
-elif FLAGS.model == 'rat_pfd':
-    support = pfd_rational(adj, FLAGS.max_degree)
-    num_supports = 1
-    model_func = RAT
-    print('rational_pfd')
-else:
-    raise ValueError('Invalid argument for model: ' + str(FLAGS.model))
-
-# Define placeholders
-placeholders = dict()
-if not approx_eigen:
-    placeholders = {
-        'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
-        'features': tf.sparse_placeholder(tf.float32, shape=tf.constant(features[2], dtype=tf.int64)),
-        'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
-        'labels_mask': tf.placeholder(tf.int32),
-        'dropout': tf.placeholder_with_default(0., shape=()),
-        'num_features_nonzero': tf.placeholder(tf.int32)  # helper variable for sparse dropout
-    }
-else:
-    placeholders = {
-        'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
-        'support_inv': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
-        # 'eigen_vec': tf.placeholder(tf.float32, shape=(None, FLAGS.eig_dim)),
-        'features': tf.sparse_placeholder(tf.float32, shape=tf.constant(features[2], dtype=tf.int64)),
-        'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
-        'labels_mask': tf.placeholder(tf.int32),
-        'dropout': tf.placeholder_with_default(0., shape=()),
-        'num_features_nonzero': tf.placeholder(tf.int32)  # helper variable for sparse dropout
-    }
-
-# Create model
-model = model_func(placeholders, input_dim=features[2][1], logging=True)
-
-# Initialize session
-sess = tf.Session()
-
 
 # Define model evaluation function
 def evaluate(features, support, labels, mask, placeholders, approx_eigen=False, support_inv=None):
@@ -111,78 +21,6 @@ def evaluate(features, support, labels, mask, placeholders, approx_eigen=False, 
     outs_val = sess.run([model.loss, model.accuracy], feed_dict=feed_dict_val)
     return outs_val[0], outs_val[1], (time.time() - t_test)
 
-
-# Init variables
-sess.run(tf.global_variables_initializer())
-
-logdir = 'tflog/'
-tb_write = tf.summary.FileWriter(logdir)
-tb_write.add_graph(sess.graph)
-
-cost_val = []
-
-# Train model
-for epoch in range(FLAGS.epochs):
-
-    t = time.time()
-    # Construct feed dictionary
-    feed_dict = construct_feed_dict(features, support, y_train, train_mask, placeholders)
-    feed_dict.update({placeholders['dropout']: FLAGS.dropout})
-
-    # Training step
-    outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
-
-    # Validation
-    cost, acc, duration = evaluate(features, support, y_val, val_mask, placeholders)
-    cost_val.append(cost)
-
-    # Print results
-    print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(outs[1]),
-          "train_acc=", "{:.5f}".format(outs[2]), "val_loss=", "{:.5f}".format(cost),
-          "val_acc=", "{:.5f}".format(acc), "time=", "{:.5f}".format(time.time() - t))
-
-    if epoch > FLAGS.early_stopping and cost_val[-1] > np.mean(cost_val[-(FLAGS.early_stopping + 1):-1]):
-        print("Early stopping...")
-        break
-
-print("Optimization Finished!")
-
-# rational training after gcn
-print('Start rational training...')
-if True:
-    rat_model = RAT_after_GCN(placeholders, input_dim=features[2][1], gcn=model, support_inv=support_inv, logging=True)
-    model = rat_model
-    sess.run(tf.global_variables_initializer())
-
-    for epoch in range(FLAGS.epochs):
-
-        t = time.time()
-        # Construct feed dictionary
-        feed_dict = construct_feed_dict(features, support, y_train, train_mask, placeholders, support_inv=None,
-                                        approx_eigen=True)
-        feed_dict.update({placeholders['dropout']: FLAGS.dropout})
-
-        # Training step
-        outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
-
-        # Validation
-        cost, acc, duration = evaluate(features, support, y_val, val_mask, placeholders, approx_eigen=True,
-                                       support_inv=None)
-        cost_val.append(cost)
-
-        # Print results
-        print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(outs[1]),
-              "train_acc=", "{:.5f}".format(outs[2]), "val_loss=", "{:.5f}".format(cost),
-              "val_acc=", "{:.5f}".format(acc), "time=", "{:.5f}".format(time.time() - t))
-
-        if epoch > FLAGS.early_stopping and cost_val[-1] > np.mean(cost_val[-(FLAGS.early_stopping + 1):-1]):
-            print("Early stopping...")
-            break
-
-    print("Rational Optimization Finished!")
-
-
-# Testing
 
 def evaluate_roc(features, support, labels, mask, placeholders, name='model', approx_eigen=False):
     t_test = time.time()
@@ -254,7 +92,183 @@ def evaluate_roc(features, support, labels, mask, placeholders, name='model', ap
     return loss, acc, (time.time() - t_test)
 
 
-test_cost, test_acc, test_duration = evaluate_roc(features, support, y_test, test_mask, placeholders, name=FLAGS.model,
-                                                  approx_eigen=approx_eigen)
-print("Test set results:", "cost=", "{:.5f}".format(test_cost),
-      "accuracy=", "{:.5f}".format(test_acc), "time=", "{:.5f}".format(test_duration))
+if __name__ == '__main__':
+
+    # Set random seed
+    seed = 88
+    np.random.seed(seed)
+    tf.set_random_seed(seed)
+
+    # Settings
+    flags = tf.app.flags
+    FLAGS = flags.FLAGS
+    flags.DEFINE_string('dataset', 'pubmed', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed', 'simu'
+    flags.DEFINE_string('model', 'gcn_cheby', 'Model string.')  # 'gcn', 'gcn_cheby', 'dense', 'rat'
+    flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+    flags.DEFINE_integer('epochs', 200, 'Number of epochs to train.')
+    flags.DEFINE_integer('hidden1', 16, 'Number of units in hidden layer 1.')
+    flags.DEFINE_float('dropout', 0.5, 'Dropout rate (1 - keep probability).')
+    flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
+    flags.DEFINE_integer('early_stopping', 50, 'Tolerance for early stopping (# of epochs).')
+    flags.DEFINE_integer('max_degree', 3, 'Maximum Chebyshev polynomial degree.')
+    flags.DEFINE_integer('eig_dim', 400, 'Maximum eigen value number.')
+
+    # Load data
+    adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(FLAGS.dataset)
+
+    support_inv = []
+    approx_eigen = True
+
+    # Some preprocessing
+    features = preprocess_features(features)
+    if FLAGS.model == 'gcn':
+        support = [preprocess_adj(adj)]
+        num_supports = 1
+        model_func = GCN
+        print('gcn')
+    elif FLAGS.model == 'gcn_cheby':
+        support = chebyshev_polynomials(adj, FLAGS.max_degree)
+        support_inv = chebyshev_polynomials_inv(adj, FLAGS.max_degree)
+        num_supports = 1 + FLAGS.max_degree
+        model_func = GCN
+        print('gcn_cheby')
+    elif FLAGS.model == 'dense':
+        support = [preprocess_adj(adj)]  # Not used
+        num_supports = 1
+        model_func = MLP
+        print('dense')
+    elif FLAGS.model == 'rat':
+        support = normal_rational(adj, FLAGS.max_degree)
+        num_supports = 1 + FLAGS.max_degree
+        model_func = RAT
+        print('rational')
+    elif FLAGS.model == 'rat_element':
+        support = element_rational(adj, FLAGS.max_degree, eig_dim=FLAGS.eig_dim)
+        num_supports = 1 + FLAGS.max_degree
+        model_func = RAT_ELEMENT
+        print('rational')
+    elif FLAGS.model == 'rat_pfd':
+        support = pfd_rational(adj, FLAGS.max_degree)
+        num_supports = 1
+        model_func = RAT
+        print('rational_pfd')
+    else:
+        raise ValueError('Invalid argument for model: ' + str(FLAGS.model))
+
+    # Define placeholders
+    placeholders = dict()
+    if not approx_eigen:
+        placeholders = {
+            'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
+            'features': tf.sparse_placeholder(tf.float32, shape=tf.constant(features[2], dtype=tf.int64)),
+            'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
+            'labels_mask': tf.placeholder(tf.int32),
+            'dropout': tf.placeholder_with_default(0., shape=()),
+            'num_features_nonzero': tf.placeholder(tf.int32)  # helper variable for sparse dropout
+        }
+    else:
+        placeholders = {
+            'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
+            'support_inv': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
+            # 'eigen_vec': tf.placeholder(tf.float32, shape=(None, FLAGS.eig_dim)),
+            'features': tf.sparse_placeholder(tf.float32, shape=tf.constant(features[2], dtype=tf.int64)),
+            'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
+            'labels_mask': tf.placeholder(tf.int32),
+            'dropout': tf.placeholder_with_default(0., shape=()),
+            'num_features_nonzero': tf.placeholder(tf.int32)  # helper variable for sparse dropout
+        }
+
+    # Create model
+    model = model_func(placeholders, input_dim=features[2][1], logging=True)
+
+    # Initialize session
+    sess = tf.Session()
+
+    # Init variables
+    sess.run(tf.global_variables_initializer())
+
+    logdir = 'tflog/'
+    tb_write = tf.summary.FileWriter(logdir)
+    tb_write.add_graph(sess.graph)
+
+    cost_val = []
+
+    # Train model
+    # ################################
+    # GCN training
+    # ################################
+    for epoch in range(FLAGS.epochs):
+
+        t = time.time()
+        # Construct feed dictionary
+        feed_dict = construct_feed_dict(features, support, y_train, train_mask, placeholders)
+        feed_dict.update({placeholders['dropout']: FLAGS.dropout})
+
+        # Training step
+        outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
+
+        # Validation
+        cost, acc, duration = evaluate(features, support, y_val, val_mask, placeholders)
+        cost_val.append(cost)
+
+        # Print results
+        print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(outs[1]),
+              "train_acc=", "{:.5f}".format(outs[2]), "val_loss=", "{:.5f}".format(cost),
+              "val_acc=", "{:.5f}".format(acc), "time=", "{:.5f}".format(time.time() - t))
+
+        if epoch > FLAGS.early_stopping and cost_val[-1] > np.mean(cost_val[-(FLAGS.early_stopping + 1):-1]):
+            print("Early stopping...")
+            break
+
+    print("Optimization Finished!")
+
+    test_cost, test_acc, test_duration = evaluate_roc(features, support, y_test, test_mask, placeholders,
+                                                      name=FLAGS.model,
+                                                      approx_eigen=approx_eigen)
+    print("Test set results:", "cost=", "{:.5f}".format(test_cost),
+          "accuracy=", "{:.5f}".format(test_acc), "time=", "{:.5f}".format(test_duration))
+
+    # ################################
+    # rational training after gcn
+    # ################################
+    print('Start rational training...')
+    if True:
+        rat_model = RAT_after_GCN(placeholders, input_dim=features[2][1], gcn=model, support_inv=support_inv,
+                                  logging=True)
+        model = rat_model
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(FLAGS.epochs):
+
+            t = time.time()
+            # Construct feed dictionary
+            feed_dict = construct_feed_dict(features, support, y_train, train_mask, placeholders, support_inv=None,
+                                            approx_eigen=True)
+            feed_dict.update({placeholders['dropout']: FLAGS.dropout})
+
+            # Training step
+            # outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
+
+            # Validation
+            cost, acc, duration = evaluate(features, support, y_val, val_mask, placeholders, approx_eigen=True,
+                                           support_inv=None)
+            cost_val.append(cost)
+
+            # Print results
+            print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(outs[1]),
+                  "train_acc=", "{:.5f}".format(outs[2]), "val_loss=", "{:.5f}".format(cost),
+                  "val_acc=", "{:.5f}".format(acc), "time=", "{:.5f}".format(time.time() - t))
+
+            if epoch > FLAGS.early_stopping and cost_val[-1] > np.mean(cost_val[-(FLAGS.early_stopping + 1):-1]):
+                print("Early stopping...")
+                break
+
+        print("Rational Optimization Finished!")
+
+    # Testing
+
+    test_cost, test_acc, test_duration = evaluate_roc(features, support, y_test, test_mask, placeholders,
+                                                      name=FLAGS.model,
+                                                      approx_eigen=approx_eigen)
+    print("Test set results:", "cost=", "{:.5f}".format(test_cost),
+          "accuracy=", "{:.5f}".format(test_acc), "time=", "{:.5f}".format(test_duration))
